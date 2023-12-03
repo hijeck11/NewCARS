71822658e8471a7efba70db3b7490a360fc19011
Антон Пашкевич <pashjevich.anton.v@gmail.com>
2023-12-04 00:45:20 +0300

    New app by DjangoREST

--- cars/urls.py
+++ cars/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('test_app.urls')),
+    path('', include('rest_app.urls')),
 ]
 
 if settings.DEBUG:

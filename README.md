96a3f6a51a0935b7cce47b5dc3a231c1b997ca28
Антон Пашкевич <pashjevich.anton.v@gmail.com>
2023-12-20 14:58:35 +0300

    ДЗ на 20.12.2023 Подключить Swagger

--- README.md
+++ README.md
@@ -1,4 +1,45 @@
 # NewCARS
+ДЗ на 20.12.22 
+Подключить Swagger
+
+1. Установите drf-yasg с помощью pip:
+```
+pip install drf-yasg
+```
+2. Добавьте drf_yasg в INSTALLED_APPS в файле настроек Django:
+```
+python
+Copy code
+INSTALLED_APPS = [     # ...     'drf_yasg',     # ... ]
+```
+3. Добавьте следующий код в ваш файл urls.py:
+```
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://www.yourapp.com/terms/",
+        contact=openapi.Contact(email="contact@yourapp.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('your_api_app.urls')),  # Замените 'your_api_app' на название вашего приложения с API
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
+```
+
 ДЗ на 18.12.23
 Сделать отправку сообщений из джанго
 
--- cars/urls.py
+++ cars/urls.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+# Добавляю swagger
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://www.yourapp.com/terms/",
+        contact=openapi.Contact(email="contact@yourapp.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,6 +27,11 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
     path('', include('sendemail.urls')),
+    path("debug/", include("debug_toolbar.urls")),
+    path('api/', include('rest_app.urls')),  # Замените 'your_api_app' на название вашего приложения с API
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 if settings.DEBUG:

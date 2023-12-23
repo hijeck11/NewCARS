d96f549a2a7be0b85a0d92a61d9e021f6986a8ec
hijeck11 <pashkevich.anton.v@gmail.com>
2023-12-24 01:28:29 +0300

    Сменил постгрес на sql

--- cars/settings.py
+++ cars/settings.py
@@ -86,19 +86,24 @@ WSGI_APPLICATION = 'cars.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'NewCARS',
+#         'USER': 'anton',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+# }
+
 DATABASES = {
     'default': {
-
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'NewCARS',
-        'USER': 'anton',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
